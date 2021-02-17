7b43920a6459cb8ed4d7a329bc04896fc54b0afb
Ana Matilde Guedes Perez da Silva Barra <up201904795@fe.up.pt>
2021-02-17 16:57:25 +0000

    TP1 Final Commit

--- tp1/MyInterface.js
+++ tp1/MyInterface.js
@@ -17,12 +17,30 @@ export class MyInterface extends CGFinterface {
         // https://github.com/dataarts/dat.gui/blob/master/API.md
         this.gui = new dat.GUI();
 
+        //This enlarges the GUI to fit the bigger names
+        this.gui.width = 285;
+
         //Checkbox element in GUI
         this.gui.add(this.scene, 'displayAxis').name('Display Axis');
 
         //Slider element in GUI
         this.gui.add(this.scene, 'scaleFactor', 0.1, 5).name('Scale Factor');
 
+        //Checkbox element in GUI - Diamond
+        this.gui.add(this.scene, 'displayDiamond').name('Display Diamond');
+
+        //Checkbox element in GUI - Triangle
+        this.gui.add(this.scene, 'displayTriangle').name('Display Triangle');
+
+        //Checkbox element in GUI - Parallelogram
+        this.gui.add(this.scene, 'displayParallelogram').name('Display Parallelogram');
+
+        //Checkbox element in GUI - Triangle Small
+        this.gui.add(this.scene, 'displayTriangleSmall').name('Display TriangleSmall');
+
+        //Checkbox element in GUI - Triangle Big
+        this.gui.add(this.scene, 'displayTriangleBig').name('Display TriangleBig');
+        
         return true;
     }
 }
--- tp1/README.md
+++ tp1/README.md
@@ -1,10 +1,9 @@
 # CGRA 2020/2021
 
-## Group T0xG0y
+## Group T03G05
 
 ## TP 1 Notes
 
-(add your main observations/remarks about your experiments here, in a bulleted list, and remove this line. Some examples below)
-
-- In exercise 1 we observed X
-- In exercise 2 we had difficulties in Y
+- In exercise 1 we observed the importance of the order of the indexes to be able to display the double-sided parallelogram.
+- In exercise 2 we saw the importance of doing being alert to the numbers we wrote on the vertices vector so that we could arrive to the pretended result.
+- In the end, we studied the GUI API and found a way to increase the size of it in order to be able to display the full names of the pieces.
